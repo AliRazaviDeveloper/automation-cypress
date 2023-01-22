77522432e3fc75bf5ade3b42d527bda27c40344d
AliRazavi <razavi.ali.1998@gmail.com>
2023-01-22 13:49:12 +0330

    move github

---
+++ cypress/support/e2e.js
@@ -0,0 +1,9 @@
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+import './commands2'
+import "cypress-real-events/support";
+import "cypress-iframe"
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
